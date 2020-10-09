fbb4019e4c070e2381c04dd462b7a049759f7936
Hnooz <mohammededris23@gmail.com>
2020-10-09 17:25:11 +0200

    wip

--- resources/js/Pages/Dashboard/meals/index.vue
+++ resources/js/Pages/Dashboard/meals/index.vue
@@ -75,8 +75,9 @@
                                         <div class="flex items-center">
                                             <div class="flex-shrink-0 h-10 w-10">
                                                 <img class="h-10 w-10 rounded-full"
-                                                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                     src="https://images.unsplash.com/photo-1424847651672-bf20a4b0982b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
                                                      alt=""/>
+                                                     
                                             </div>
                                             <div class="mx-2">
                                                 <!-- <div class="text-sm leading-5 font-medium text-gray-900">{{meal.name}}</div> -->
